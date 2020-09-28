c2e6cff486a13723d28f25ac09bf1732077f4b58
CTY <42131250+CTYTC@users.noreply.github.com>
2020-09-28 17:21:19 +0800

    update on article's function
    add new function

--- Controller/articleController.js
+++ Controller/articleController.js
@@ -24,10 +24,38 @@ const createArticle = (req, res) =>{
         if (result){
             res.send("Create Article Successful")
         }else{
-            res.send('?')
+            res.send("FAIL TO CREATE")
         }
     })
 };
+
+const deleteArticle = (req, res) =>{
+    const sql ='DELETE FROM article where title = ?'
+    const data = req.body.title
+    db.base(sql, data, (result)=>{
+        if(result == 1){
+            res.send("Delete successful")
+        }else {
+            res.send("FAIL TO DELETE")
+        }
+    })
+}
+
+const updateArticle = (req,res) =>{
+    const sql = 'UPDATE article set description = ?, content = ? where title = ?'
+    const data = [
+        req.body.title,
+        req.body.description,
+        req.body.content
+    ]
+    db.base(sql, data, (result)=>{
+        if(result == 1){
+            result.send("Update Successful")
+        }else {
+            res.send("FAIL TO MODIFY")
+        }
+    })
+}
 
 
 
@@ -39,5 +67,7 @@ const getModifyPage = (req, res) =>{
 module.exports = {
     getArticlePage,
     createArticle,
-    getModifyPage
+    getModifyPage,
+    deleteArticle,
+    updateArticle
 };
--- router/articleRouter.js
+++ router/articleRouter.js
@@ -5,5 +5,10 @@ const articleController = require('../Controller/articleController');
 
 articleRouter.get('/',(req,res) => articleController.getArticlePage(req,res));
 articleRouter.post('/createArticle',(req,res) => articleController.createArticle(req,res));
-articleRouter.get('/modifyArticle', (req, res) => articleController.getModifyPage(req, res))
+articleRouter.get('/modifyArticle', (req,res) => articleController.getModifyPage(req, res));
+articleRouter.post('/deleteArticle', (req,res) => articleController.deleteArticle(req,res));
+articleRouter.post('/updateArticle', (req, res) =>articleController.updateArticle(req, res));
+
+
+
 module.exports = articleRouter;
