0cbf895fc03495c3899cbcd0891671817de99d9d
CTY <42131250+CTYTC@users.noreply.github.com>
2020-09-28 17:36:23 +0800

    Revert "test"
    This reverts commit db7dc86c9f07b2ecf0d1a4b131338d60231a5873, reversing
    changes made to c2e6cff486a13723d28f25ac09bf1732077f4b58.

--- router/loginRouter.js
+++ router/loginRouter.js
@@ -5,5 +5,6 @@ const loginController = require('../Controller/loginController');
 loginRouter.get('/',(req,res) => loginController.getLoginPage(req,res));
 loginRouter.post('/',(req,res) => loginController.loginToSystem(req,res));
 loginRouter.get('/register',(req,res) => loginController.getRegisterPage(req,res));
+loginRouter.post('/register',(req,res) => loginController.getUserInfo(req,res));
 
 module.exports = loginRouter;
