d349eb95f20ea1c3cd9cae5fc4955d556706c8c6
iggy-1217 <62122098+iggy-1217@users.noreply.github.com>
2020-10-02 18:24:35 +0800

    update year page of timeline

--- Controller/timelineController.js
+++ Controller/timelineController.js
@@ -2,16 +2,21 @@ const db = require('../model/index');
 const moment = require('moment');
 
 const getPage = (req,res) =>{
-    let sql = 'select * from timeline';
+    let sql = 'select year from timeline GROUP BY year';
     db.base(sql,null,(result) =>{
         // res.render('timelinePage',{list:result});
         res.send(result);
     })
 };
 
-const addTimelinePage = (req,res) =>{
-    res.render('addTimeline',{});
-};
+const getSpecificYearPage = (req,res) =>{
+    let sql = 'select * from timeline Where year = ? ORDER BY month ASC';
+    let year = req.body.year;
+    db.base(sql,year,(result) =>{
+        res.send(result)
+    })
+}
+
 
 const addTimeline = (req,res) =>{
     let info = req.body;
@@ -27,8 +32,23 @@ const addTimeline = (req,res) =>{
     })
 };
 
+const pageInfo = (req,res) => {
+    let {year,curr} = req.query;
+    let where = '';
+    if(year != undefined) {
+        where = `where year=${year} `
+    }
+    let sql = `select * from timeline ${where} ORDER BY month ASC Limit ${curr * 3},${curr * 3 + 3}`;
+    db.base(sql,null,(result) =>{
+        console.log(result);
+        res.send(result)
+    })
+}
+
+
+
 module.exports = {
     getPage,
-    addTimelinePage,
-    addTimeline
+    addTimeline,
+    pageInfo
 };
