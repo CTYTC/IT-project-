374bdc92ed0b8b5cdad48c277a26adea8affa488
iggy-1217 <62122098+iggy-1217@users.noreply.github.com>
2020-10-13 12:41:06 +0800

    delete master--5

--- router/timelineRouter.js
+++ router/timelineRouter.js
@@ -6,5 +6,8 @@ timelineRouter.get('/',(req,res) => timelineController.getPage(req,res));
 timelineRouter.get('/add',(req,res) => timelineController.addTimelinePage(req,res));
 timelineRouter.post('/add',(req,res) => timelineController.addTimeline(req,res));
 timelineRouter.get('/pageinfo',(req,res) => timelineController.pageInfo(req,res));
+timelineRouter.post('/delete',(req,res) => timelineController.deleteTimeline(req,res));
+timelineRouter.post('/update',(req,res) => timelineController.updateTimeline(req,res));
+timelineRouter.get('/getAllInfo',(req,res) => timelineController.getAllInfo(req,res));
 
 module.exports = timelineRouter;
