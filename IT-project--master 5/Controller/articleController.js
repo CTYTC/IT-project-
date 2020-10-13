2e4ff065019603b0a09f044ed711f07afe0330c9
iggy-1217 <62122098+iggy-1217@users.noreply.github.com>
2020-10-13 12:03:22 +0800

    timeline update and delete function

---
+++ IT-project--master 5/Controller/articleController.js
@@ -0,0 +1,87 @@
+const db = require('../model/index');
+const sd = require("silly-datetime")
+
+
+const getArticlePage = (req,res) => {
+    const sql = 'SELECT * FROM article ORDER BY createDate DESC';
+    const data = [];
+    db.base(sql, data, (result)=>{
+        res.send(result)
+    })
+};
+
+const viewArticle = (req,res) =>{
+    console.log(req.body)
+    const sql = 'SELECT * FROM article WHERE title = ?'
+    const data = [req.body]
+    db.base(sql, data, (result)=> {
+        res.send(result)
+    })
+}
+
+const createArticlePage = (req, res)=>{
+    res.render('createArticle', {})
+}
+
+const createArticle = (req, res) =>{
+    const sql = 'INSERT INTO article (title, createDate, description, content) VALUE (?, ?, ?, ?)'
+    const time = sd.format(new Date(), 'YYYY-MM-DD HH:mm');
+    const data = [
+        req.body.title,
+        time,
+        req.body.description,
+        req.body.content
+    ]
+    db.base(sql, data, (result)=>{
+        if (result.affectedRows == 1){
+            res.send("Create Article Successful")
+        }
+    })
+};
+
+const deleteArticle = (req, res) =>{
+    const sql ='DELETE FROM article where title = ?'
+    const data = [req.body.title]
+    db.base(sql, data, (result)=>{
+        if(result.affectedRows == 1){
+            res.send("Delete successful")
+        }else {
+            res.send("FAIL TO DELETE")
+        }
+    })
+}
+
+const updateArticle = (req,res) =>{
+    const sql = 'UPDATE article set createDate = ?, description = ?, content = ? where title = ?'
+    const time = sd.format(new Date(), 'YYYY-MM-DD HH:mm');
+    const data = [
+        time,
+        req.body.description,
+        req.body.content,
+        req.body.title
+    ]
+    db.base(sql, data, (result)=>{
+        if(result == 1){
+            result.send("Update Successful")
+        }else {
+            res.send("FAIL TO MODIFY")
+        }
+    })
+}
+
+
+
+
+const getModifyPage = (req, res) =>{
+    res.render('modifyArticle')
+}
+
+module.exports = {
+    getArticlePage,
+    createArticle,
+    getModifyPage,
+    deleteArticle,
+    updateArticle,
+    createArticlePage,
+    viewArticle
+};
